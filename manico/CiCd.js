2d49660d50b753994876efca5f0d312d953cff8a
Aryan Singh <aryan.singh.en.1142@gmail.com>
2026-07-12 16:10:16 +0530

    feat: initialize project scaffolding with CI/CD pipeline and automated testing scripts

---
+++ manico/CiCd.js
@@ -0,0 +1,1 @@
+console.log('Checks Continuous Integration & Deployment')
---
+++ manico/test.js
@@ -0,0 +1,5 @@
+console.log('Start testing......')
+setTimeout((()=> {
+    console.log('Starting server in 3 seconds....')
+}),3000)
+console.log('Test complete!')
